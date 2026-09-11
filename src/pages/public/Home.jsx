e48cbe1c9f850a6a8997629fb679c027709edbe6
Rosário Pompilio Caravela <rosariocaravela116@gmail.com>
2026-09-12 00:28:25 +0200

    feat: Atualiza página inicial

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <main className="min-h-screen bg-blue-100 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-700">
-        Suporte aos Trabalhos
+        Suporte aos Trabalhos-rosario Caravela
       </h1>
     </main>
   )
