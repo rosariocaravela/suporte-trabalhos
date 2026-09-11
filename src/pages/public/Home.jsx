9fa1c8c297602ada5447c6d447e25da5b9ac0aeb
Rosário Pompilio Caravela <rosariocaravela116@gmail.com>
2026-09-11 16:25:00 +0200

    feat: configurar rotas da aplicação

---
+++ src/pages/public/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <main className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-700">
+        Suporte aos Trabalhos
+      </h1>
+    </main>
+  )
+}
+
+export default Home
